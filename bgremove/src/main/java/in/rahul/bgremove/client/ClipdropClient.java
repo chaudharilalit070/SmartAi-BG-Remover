22d81b6c182e7f2b56a0344bc0becd72ff05d1e0
chaudharilalit070 <chaudharilalit717@gmail.com>
2025-06-10 21:32:47 +0530

    Update ClipdropClient.java

--- bgremove/src/main/java/in/rahul/bgremove/client/ClipdropClient.java
+++ bgremove/src/main/java/in/rahul/bgremove/client/ClipdropClient.java
@@ -1,19 +1,4 @@
-//package in.rahul.bgremove.client;
-//
-//import org.springframework.cloud.openfeign.FeignClient;
-//import org.springframework.http.MediaType;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestHeader;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RequestPart;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//@FeignClient(name="ClipdropClient" ,url="https://clipdrop-api.co")
-//public interface ClipdropClient {
-//    @PostMapping(value = "/remove-background/v1",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//      byte[] removeBackGround(@RequestPart("image_file") MultipartFile file,
-//                        @RequestHeader("x-api-key") String apiKey);
-//}
+
 package in.rahul.bgremove.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
