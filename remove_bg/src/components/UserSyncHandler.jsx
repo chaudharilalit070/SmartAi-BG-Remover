05c8564e300587f8d5be9db8a3edfc7263fba920
chaudharilalit070 <chaudharilalit717@gmail.com>
2025-06-10 21:50:41 +0530

    Update UserSyncHandler.jsx

--- remove_bg/src/components/UserSyncHandler.jsx
+++ remove_bg/src/components/UserSyncHandler.jsx
@@ -1,59 +1,3 @@
-// const { useAuth, useUser } = require("@clerk/clerk-react");
-// const { useState, useEffect, useContext } = require("react");
-// const { AppContext } = require("../context/AppContext");
-// const { default: axios } = require("axios");
-// const { default: toast } = require("react-hot-toast");
-
-// const UserSyncHandler=()=>{
-// const {isLoaded,isSingedIn,getToken}=useAuth();
-// const {user}=useUser();
-// const [synced ,setSynced]=useState(false);
-// const {backendUrl} =useContext(AppContext)
-// useEffect(()=>{
-
-//     const saveUser=async()=>{
-//         if (!isLoaded|| !isSingedIn||synced) {
-//             return;
-
-//         }
-//         try {
-//           const token=await getToken();
-//           const userData={
-//             clerkId:user.id,
-//             email:user.primaryEmailAddress.emailAddress,
-//             firstName:user.firstName,
-//             lastName:user.lastName,
-
-
-//           };
-// const response= axios.post(backendUrl+"/users",userData,{headers:{"Authorization":`Beaere ${token}`}});         
-// if (response.data.success === true ) {
-//     console.log("user successfuly created !");
-//     toast.success("user successfuly created !")
-
-// }
-// else{
-// toast.error("user sycn feild. please try again")
-
-// }
-// setSynced(true)//prevent re-posting
-//         } catch (error) {
-//             console.error("user sycn feild",error);
-//             toast.error("unable to create account. please try again")
-
-
-//         }
-//     }
-//     saveUser(); 
-// },[isLoaded,isSingedIn,getToken,user,synced]);
-
-
-//     return null;
-// }
-
-// export default UserSyncHandler;
-
-
 import { useAuth, useUser } from "@clerk/clerk-react";
 import { useState, useEffect, useContext } from "react";
 import { AppContext } from "../context/AppContext";
@@ -115,5 +59,3 @@ const UserSyncHandler = () => {
 
 export default UserSyncHandler;
 
-
-//7:2 min
